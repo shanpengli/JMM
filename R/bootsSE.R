80f24ce87afa820f51c247a7050a5e4b574e6643
Shanpeng Li <shanpengli@Shanpengs-Mac-mini.local>
2021-02-18 12:51:49 -0800

    update boots

--- R/bootsSE.R
+++ R/bootsSE.R
@@ -43,15 +43,17 @@ bootsSE <- function(object, nboots = 100, print.para = FALSE, maxiter = 1000,
   
   t_max = max(mdata[, 1])
   
+  Fnboots <- nboots*2
+  
   Data = bootsdata_main(n, n1, tL, tU, q_eta, j_max, p01, p02, t_max, 
-                            ydatanew, cdatanew, mdatanew, nboots)
+                            ydatanew, cdatanew, mdatanew, Fnboots)
   
   nycol <- dim(object$ydata)[2]
   nccol <- dim(object$cdata)[2]
   
   ## Allocate a matrix for bootstrap sample estimates
   ncolM <- j_max*p_max + 2*j_max + q_b + k_max + q_eta + k_max*q_b + 1
-  ParaMatrix <- matrix(0, nrow = nboots, ncol = ncolM)
+  ParaMatrix <- matrix(0, nrow = Fnboots, ncol = ncolM)
   ParaMatrix <- as.data.frame(ParaMatrix)
   
   ## Initialize the parameter estimates
@@ -63,12 +65,11 @@ bootsSE <- function(object, nboots = 100, print.para = FALSE, maxiter = 1000,
   tgamma <- 0.26
   
   
-  
+  Realboot <- 0
   
   if (ncores == 1)
   {
-    for (i in 1:nboots) {
-      writeLines(paste0("Running ", i, " th sample!"))
+    for (i in 1:Fnboots) {
       bootsydata <- matrix(unlist(Data[1+(i-1)*3]), ncol = nycol)
       bootsydata <- as.data.frame(bootsydata)
       bootscdata <- matrix(unlist(Data[2+(i-1)*3]), ncol = nccol)
@@ -76,83 +77,88 @@ bootsSE <- function(object, nboots = 100, print.para = FALSE, maxiter = 1000,
       bootsmdata <- matrix(unlist(Data[3*i]), ncol = 1)
       bootsmdata <- as.data.frame(bootsmdata)
 
-      fit <- try(jmspline(ydata = bootsydata, cdata = bootscdata, mdata = bootsmdata,
+      fit <- jmspline(ydata = bootsydata, cdata = bootscdata, mdata = bootsmdata,
                           sigmau_inv = sigmau_inv, tbtheta = tbtheta, tL = tL, tU = tU,
                           nbreak = nbreak, p01 = p01, p02 = p02, j_max = j_max,
                           k_max = k_max, quadpoint = quadpoint, maxiter = maxiter,
                           do.trace = FALSE, beta0init = tbeta0, beta1init = tbeta1,
                           sigmainit = tsigma2, thetainit = ttheta, sigmadinit = tsigmad,
-                          gammainit = tgamma), silent = TRUE)
+                          gammainit = tgamma)
       
-      if ('try-error' %in% class(fit)) {
+      if (fit$iter == maxiter) {
         ParaMatrix[i, ] <- NA
       } else {
-            beta0 <- fit$beta0_matrix
-            beta1 <- fit$beta1_estimate
-            sigma2 <- fit$sigma2_estimate
-            theta <- fit$theta_estimate
-            sigmad <- fit$sigmad_estimate
-            eta <- fit$eta_estimate
-            btheta <- fit$btheta_matrix
-            gamma <- fit$gamma
-            
-            ##ncolM <- j_max*p_max + 2*j_max + q_b + k_max + q_eta + k_max*q_b + 1
-            pp = 1
-            for (t in 1:j_max) {
-              for (u in 1:p_max) {
-                ParaMatrix[i, pp] = beta0[t, u]
-                if (i == 1) colnames(ParaMatrix)[pp] <- paste0("beta0_", t, u)
-                pp = pp+1
-              }
-            }
-            
-            for (t in 1:j_max) {
-              ParaMatrix[i, pp] = beta1[t]
-              if (i == 1) colnames(ParaMatrix)[pp] <- paste0("beta1_", t)
-              pp = pp+1
-            }
-            
-            for (t in 1:j_max) {
-              ParaMatrix[i, pp] = sigma2[t]
-              if (i == 1) colnames(ParaMatrix)[pp] <- paste0("sigma2_", t)
-              pp = pp+1
-            }
-            
-            for (t in 1:q_b) {
-              ParaMatrix[i, pp] = theta[t]
-              if (i == 1) colnames(ParaMatrix)[pp] <- paste0("theta_", t)
-              pp = pp+1
-            }
-            
-            for (t in 1:k_max) {
-              ParaMatrix[i, pp] = sigmad[t]
-              if (i == 1) colnames(ParaMatrix)[pp] <- paste0("sigmad_", t)
-              pp = pp+1
-            }
-            
-            for (t in 1:q_eta) {
-              ParaMatrix[i, pp] = eta[t]
-              if (i == 1) colnames(ParaMatrix)[pp] <- paste0("eta_", t)
-              pp = pp+1
-            }
-            
-            for (t in 1:k_max) {
-              for (u in 1:q_b) {
-                ParaMatrix[i, pp] = btheta[u, t]
-                if (i == 1) colnames(ParaMatrix)[pp] <- paste0("btheta_", u, t)
-                pp = pp+1
-              }
-            }
-            
-            ParaMatrix[i, pp] = gamma
-            if (i == 1) colnames(ParaMatrix)[pp] <- "gamma"
-            
-            if (print.para == TRUE) {
-              print(ParaMatrix[i, ])
-            }
+        Realboot <- Realboot + 1
+        writeLines(paste0(Realboot, " th sample is done!"))
+        beta0 <- fit$beta0_matrix
+        beta1 <- fit$beta1_estimate
+        sigma2 <- fit$sigma2_estimate
+        theta <- fit$theta_estimate
+        sigmad <- fit$sigmad_estimate
+        eta <- fit$eta_estimate
+        btheta <- fit$btheta_matrix
+        gamma <- fit$gamma
         
+        ##ncolM <- j_max*p_max + 2*j_max + q_b + k_max + q_eta + k_max*q_b + 1
+        pp = 1
+        for (t in 1:j_max) {
+          for (u in 1:p_max) {
+            ParaMatrix[i, pp] = beta0[t, u]
+            colnames(ParaMatrix)[pp] <- paste0("beta0_", t, u)
+            pp = pp+1
+          }
+        }
+        
+        for (t in 1:j_max) {
+          ParaMatrix[i, pp] = beta1[t]
+          colnames(ParaMatrix)[pp] <- paste0("beta1_", t)
+          pp = pp+1
+        }
+        
+        for (t in 1:j_max) {
+          ParaMatrix[i, pp] = sigma2[t]
+          colnames(ParaMatrix)[pp] <- paste0("sigma2_", t)
+          pp = pp+1
+        }
+        
+        for (t in 1:q_b) {
+          ParaMatrix[i, pp] = theta[t]
+          colnames(ParaMatrix)[pp] <- paste0("theta_", t)
+          pp = pp+1
+        }
+        
+        for (t in 1:k_max) {
+          ParaMatrix[i, pp] = sigmad[t]
+          colnames(ParaMatrix)[pp] <- paste0("sigmad_", t)
+          pp = pp+1
+        }
+        
+        for (t in 1:q_eta) {
+          ParaMatrix[i, pp] = eta[t]
+          colnames(ParaMatrix)[pp] <- paste0("eta_", t)
+          pp = pp+1
+        }
+        
+        for (t in 1:k_max) {
+          for (u in 1:q_b) {
+            ParaMatrix[i, pp] = btheta[u, t]
+            colnames(ParaMatrix)[pp] <- paste0("btheta_", u, t)
+            pp = pp+1
+          }
+        }
+        
+        ParaMatrix[i, pp] = gamma
+        colnames(ParaMatrix)[pp] <- "gamma"
+        
+        if (print.para == TRUE) {
+          print(ParaMatrix[i, ])
+        }
       }
+      
+      if (Realboot == nboots) break
+      
     }
+    ParaMatrix <- ParaMatrix[complete.cases(ParaMatrix), ]
     return(ParaMatrix)
   } 
   
